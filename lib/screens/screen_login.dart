244c702b12bcac5fbbb6e3d52c73ba51f930c11e
sanin <saninthottungalhere@gmail.con>
2024-05-19 16:12:20 +0530

    login UI done

--- lib/screens/screen_login.dart
+++ lib/screens/screen_login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
 
 class ScreenLogin extends StatelessWidget {
@@ -12,15 +11,42 @@ class ScreenLogin extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Authentication"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Lottie.asset(
-            'assets/login.json',
-            height: 300,
-            width: double.infinity,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Lottie.asset(
+                'assets/login.json',
+                height: 300,
+                width: double.infinity,
+              ),
+              const SizedBox(height: 50),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  focusedBorder: OutlineInputBorder(),
+                ),
+                onSubmitted: (value) {},
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  focusedBorder: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.security_outlined),
+                ),
+                obscureText: true,
+                onSubmitted: (value) {},
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Login'),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
